a4eb8626c20662e4403c86a88ffb4e790fb634d0
andrijast <andrija.stevanovic56@gmail.com>
2023-06-03 17:03:12 +0200

    import search app

--- search/views.py
+++ search/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from offer.models import Offer
+from account.forms import SignupForm
+from django.db.models import Q
 
-# Create your views here.
+
+def index(request):
+    offers = Offer.objects.filter(is_premium=True)[0:6]
+
+    return render(request, 'search/home.html', {
+        'offers': offers
+    })
+
+def browse(request):
+    query = request.GET.get('query', '')
+    offers = Offer.objects.filter()
+
+    if query:
+        offers = offers.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'search/browse.html', {
+        'offers': offers,
+        'query': query,
+    })
