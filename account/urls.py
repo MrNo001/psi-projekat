0e51f3a3c47453006d1ed31aa5c56eb5c87614ce
andrijast <andrija.stevanovic56@gmail.com>
2023-06-03 16:59:41 +0200

    import account app

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,8 @@ app_name = 'account'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('signup/', views.signup, name='signup'),
+    path('registracija/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html', authentication_form=LoginForm), name='login'),
-    path('dashboard/',views.dashboard,name='dashboard'),
+    path('profil/',views.dashboard,name='dashboard'),
     path('logout/',views.logoutuser,name='logoutuser')
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .forms import SignupForm
+from offer.models import Offer
+from search import views as v
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/account/login/', permanent=True)
+    else:
+        form = SignupForm()
+
+    return render(request, 'account/signup.html', {
+        'form': form
+    })
+
+
+@login_required
+
+def dashboard(request):
+    offers = Offer.objects.filter(created_by=request.user)
+
+    return render(request, 'account/dashboard.html', {
+        'offers': offers,
+    })
+
+
+def logoutuser(request):
+    logout(request)
+    return v.index(request)
+    
