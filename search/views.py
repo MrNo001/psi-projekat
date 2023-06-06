6900f17caf0a758aca03a6ffbdefa1e30588015b
Niko <kn200348d@student.etf.bg.ac.rs>
2023-06-06 16:07:59 +0200

    improved search feature

--- search/views.py
+++ search/views.py
@@ -14,10 +14,25 @@ def index(request):
 
 def browse(request):
     query = request.GET.get('query', '')
+    make = request.GET.get('make','')
+    model = request.GET.get('model','')
+    yearStart = request.GET.get('yearStart','')
+    yearEnd = request.GET.get('yearEnd','')
+
     offers = Offer.objects.filter()
+
 
     if query:
         offers = offers.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    if make:
+        offers = offers.filter(make__icontains=make)
+    if model:
+        offers = offers.filter(model__icontains=model)
+    if yearStart:
+        offers = offers.filter(year__gte=yearStart)
+    if yearEnd:
+        offers = offers.filter(year__lte=yearEnd)
+    
 
     packed = []
     for offer in offers:
@@ -30,6 +45,10 @@ def browse(request):
         'offers': packed,
         # 'images': images,
         'query': query,
+        'make':make,
+        'model':model,
+        'yearStart':yearStart,
+        'yearEnd':yearEnd,   
     })
 
 def about(request):
